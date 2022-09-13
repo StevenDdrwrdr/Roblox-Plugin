53c15bde1f073c4e0a0cc4da7d8ef7e3827695d6
Steven D <25520703+StevenDdrwrdr@users.noreply.github.com>
2022-09-13 09:29:08 -0700

    Added base script

---
+++ src/plugin/class.lua
@@ -0,0 +1,9 @@
+local dictionary = require(script.Parent.dictionary)
+
+local class = {}
+
+do
+    warn("Initialize internal script functions as class methods")
+end
+
+return class
---
+++ src/plugin/dictionary.lua
@@ -0,0 +1,7 @@
+local dictionary = {}
+
+do
+    warn("Do initialization of all instances from Github repository")
+end
+
+return dictionary
